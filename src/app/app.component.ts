1be1a4e05ab0dd6d635a77d311d11e79cf465d8c
cbracamonte <carlosbracamonte23@gmail.com>
2022-05-10 11:23:59 -0500

    funcion de agregar y eliminar poligonos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -23,26 +23,30 @@ export class AppComponent {
   };
   markerOptions: google.maps.MarkerOptions = { draggable: false };
   markerPositions: google.maps.LatLngLiteral[] = [];
-  defaultCabPositions = [{ lat: -8.0969728, lng: -79.0462464, color: 'blue' }];
+  defaultCabPositions = [{ lat: -8.0969728, lng: -79.0462464}];
   propsH3 = {
     kringSize: 1,
     lat: -8.0969728,
     lng: -79.0462464,
     resolution: 9,
-    plantingMode: 'RIDER',
-    cabPositions: this.defaultCabPositions,
-    riderH3Index: '',
+    h3Index: '',
     hexagons: [''],
     polyline: [{ lat: 0, lng: 0 }],
   };
+  propsH3Clone = { ...this.propsH3 };
   polygonH3Options: google.maps.PolygonOptions = {
     visible: true,
-    strokeColor: '#FF0000',
-    strokeOpacity: 0.8,
-    strokeWeight: 2,
-    fillColor: '#FF0000',
-    fillOpacity: 0.35,
+    fillColor: '#2e2e2e',
+    fillOpacity: 0.5,
+    strokeWeight: 3
   };
+  polygonH3OptionsClone: google.maps.PolygonOptions = {
+    visible: true,
+    fillColor: '#a2kk22',
+    fillOpacity: 0.2,
+    strokeWeight: 1
+
+  }
 
   constructor(httpClient: HttpClient) {
     this.setDefaultPositions();
@@ -56,8 +60,7 @@ export class AppComponent {
         catchError(() => of(false))
       );
 
-    this.getIdx();
-    this.getRiderH3();
+    this.getH3();
     this.getHexagons();
   }
 
@@ -66,29 +69,26 @@ export class AppComponent {
       const element: any = this.defaultCabPositions[index];
       element.lat = this.propsH3.lat;
       element.lng = this.propsH3.lng;
-      element.color = 'blue';
       this.defaultCabPositions.push(element);
     }
-    console.log(this.defaultCabPositions);
   }
 
-  getIdx() {
-    return geoToH3(this.propsH3.lat, this.propsH3.lng, this.propsH3.resolution);
-  }
 
-  getRiderH3() {
-    this.propsH3.riderH3Index = geoToH3(
+  getH3() {
+    this.propsH3.h3Index = geoToH3(
       this.propsH3.lat,
       this.propsH3.lng,
       this.propsH3.resolution
     );
+    this.propsH3Clone.h3Index = this.propsH3.h3Index;
   }
 
   getHexagons() {
     this.propsH3.hexagons = h3KRing(
-      this.propsH3.riderH3Index,
-      this.propsH3.kringSize
+      this.propsH3.h3Index,
+      1
     );
+    this.propsH3Clone.hexagons = this.propsH3.hexagons;
   }
 
   h3ToPolyline(h3idx: any) {
@@ -96,13 +96,13 @@ export class AppComponent {
     hexBoundary.push(hexBoundary[0]);
     let arr = [];
     for (const i of hexBoundary) {
-      arr.push({ lat: i[0], lng: i[1], color: '#d2e2de' });
+      arr.push({ lat: i[0], lng: i[1] });
     }
 
     return arr;
   }
 
-  setPolygon() {}
+  setPolygon() { }
 
   getRandomInt(max: number) {
     return Math.floor(Math.random() * Math.floor(max));
@@ -112,10 +112,14 @@ export class AppComponent {
     this.markerPositions.push(event.latLng.toJSON());
   }
 
-  polylineClick(event: any, hex: string) {
-    /*this.propsH3.hexagons = this.propsH3.hexagons.filter(
-      (hexagon) => hexagon !== hex
-    );*/
-    console.log(this.propsH3);
+  polylineClick(event: any, hex: string, isClone: boolean, index?: number) {
+    let indexHexagon = this.propsH3Clone.hexagons.findIndex(hexagon => hexagon === hex);
+    if (!isClone) {
+      this.propsH3.hexagons = this.propsH3.hexagons.filter(
+        (hexagon) => hexagon !== hex
+      );
+    } else {
+      this.propsH3.hexagons.splice(indexHexagon, 0, hex);
+    }
   }
 }
